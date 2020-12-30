bc49140fd5da10e4f88dd9c3f7e82090a37c42f7
Gilles Arcas <gilles.arcas@gmail.com>
2020-12-30 19:36:50 +0100

    Refactor with intcode object

--- 2019/07.py
+++ 2019/07.py
@@ -7,44 +7,35 @@ DATA = '07.txt'
 
 def try_phase_setting(code, setting):
     # setting = (a, b, c, d, e)
-    print(setting)
+    amp = [None] * 5
+    for ampnum in range(5):
+        amp[ampnum] = intcode.Intcode(code)
+
     inval = 0
     for ampnum in range(5):
-        print('amp', ampnum)
         phase = setting[ampnum]
-        outval, _, _ = intcode.run_code(code, [phase, inval])
+        outval = amp[ampnum].run([phase, inval])
         inval = outval
     return inval
 
 
 def try_loop_phase_setting(code, setting):
     # setting = (a, b, c, d, e)
-    print(setting)
     amp = [None] * 5
     for ampnum in range(5):
-        amp[ampnum] = [0, code[:], None]  # ptr, code, last output
+        amp[ampnum] = intcode.Intcode(code)
 
     inval = 0
     for loop in itertools.count():
         for ampnum in range(5):
             phase = setting[ampnum]
-            code = amp[ampnum][1]
-            ptr = amp[ampnum][0]
-            ## print('amp', ampnum, code[ptr])
-            if code[ptr] == 99:
-                if ampnum == 4:
-                    return amp[ampnum][2]
+            if loop == 0:
+                outval = amp[ampnum].run([phase, inval], return_output=True)
             else:
-                if loop == 0:
-                    outval, code, ptr = intcode.run_code(code, [phase, inval], ptr=ptr, return_output=True)
-                else:
-                    outval, code, ptr = intcode.run_code(code, [inval], ptr=ptr, return_output=True)
-                amp[ampnum][0] = ptr
-                amp[ampnum][1] = code
-                if outval is not None:
-                    ## print('store', ampnum, outval)
-                    amp[ampnum][2] = outval
-                inval = outval
+                outval = amp[ampnum].run([inval], return_output=True)
+            inval = outval
+        if amp[4].returned_on == 'terminate':
+            return amp[4].outvalues[-1]
 
 
 def code1():
@@ -67,19 +58,19 @@ def code2():
     print('2>', maxval)
 
 
-# code = intcode.parse_data('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0')
-# result = try_phase_setting(code, (4,3,2,1,0))
-# assert result == 43210, result
+code = intcode.parse_data('3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0')
+result = try_phase_setting(code, (4,3,2,1,0))
+assert result == 43210, result
 
-# code = intcode.parse_data('3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0')
-# result = try_phase_setting(code, (0,1,2,3,4))
-# assert result == 54321, result
+code = intcode.parse_data('3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0')
+result = try_phase_setting(code, (0,1,2,3,4))
+assert result == 54321, result
 
-# code = intcode.parse_data('3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0')
-# result = try_phase_setting(code, (1,0,4,3,2))
-# assert result == 65210, result
+code = intcode.parse_data('3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0')
+result = try_phase_setting(code, (1,0,4,3,2))
+assert result == 65210, result
 
-#code1()
+code1()
 
 code = intcode.parse_data('3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5')
 result = try_loop_phase_setting(code, (9,8,7,6,5))
--- 2019/intcode.py
+++ 2019/intcode.py
@@ -13,76 +13,88 @@ def run_strcode(strcode, initval, ptr=0, return_output=False):
     return run_code(code, initval, return_output)
 
 
-def run_code(code, initval, ptr=0, return_output=False):
-    """
-    code: list of integer instructions
-    initval: list of input values
-    return_output: return on output if True
-    return: last output, code, ptr (ptr on next instruction if return_output is True
-    """
-    print('run', initval)
-    ptr = ptr
-    outval = None
-    while True:
-        instr = code[ptr]
-        op = instr % 100
-        mode = instr // 100
-        #print('loop', instr, op)
-        if op == 1:  # add
-            i, j, dest = code[ptr + 1:ptr + 4]
-            code[dest] = opval(code, mode, i, 0) + opval(code, mode, j, 1)
-            ptr += 4
-        elif op == 2:  # mul
-            i, j, dest = code[ptr + 1:ptr + 4]
-            code[dest] = opval(code, mode, i, 0) * opval(code, mode, j, 1)
-            ptr += 4
-        elif op == 3:  # input
-            print(initval)
-            x = code[ptr + 1]
-            code[x] = initval.pop(0)
-            ptr += 2
-        elif op == 4:  # output
-            x = code[ptr + 1]
-            outval = opval(code, mode, x, 0)
-            print('output', outval)
-            ptr += 2
-            if return_output:
-                print('return_output', outval)
-                return outval, code, ptr
-        elif op == 5:  # jump-if-true
-            test, target = code[ptr + 1:ptr + 3]
-            if opval(code, mode, test, 0) != 0:
-                ptr = opval(code, mode, target, 1)
-            else:
-                ptr += 3
-        elif op == 6:  # jump-if-false
-            test, target = code[ptr + 1:ptr + 3]
-            if opval(code, mode, test, 0) == 0:
-                ptr = opval(code, mode, target, 1)
-            else:
-                ptr += 3
-        elif op == 7:  # less than
-            i, j, dest = code[ptr + 1:ptr + 4]
-            if opval(code, mode, i, 0) < opval(code, mode, j, 1):
-                code[dest] = 1
-            else:
-                code[dest] = 0
-            ptr += 4
-        elif op == 8:  # equals
-            i, j, dest = code[ptr + 1:ptr + 4]
-            if opval(code, mode, i, 0) == opval(code, mode, j, 1):
-                code[dest] = 1
-            else:
-                code[dest] = 0
-            ptr += 4
-        elif op == 99:  # terminate
-            ## print('terminate', outval)
-            return outval, code, ptr
-        else:
-            assert False
+class Intcode:
+    def __init__(self, code):
+        """
+        code: list of integer instructions
+        """
+        self.code = code[:]
+        self.ptr = 0
+        self.relbase = 0
+        self.invalues = list()
+        self.outvalues = list()
+        self.returned_on = None  # 'output' or 'terminate'
 
+    def run(self, initval, return_output=False):
+        """
+        initval: list of input values
+        return_output: return on output if True
+        return: last output
+        """
+        self.invalues.extend(initval)
+        while True:
+            instr = self.code[self.ptr]
+            op = instr % 100
+            mode = instr // 100
+            #print('loop', self.ptr, instr, op)
+            if op == 1:  # add
+                i, j, dest = self.code[self.ptr + 1:self.ptr + 4]
+                self.code[dest] = self.opval(mode, i, 0) + self.opval(mode, j, 1)
+                self.ptr += 4
+            elif op == 2:  # mul
+                i, j, dest = self.code[self.ptr + 1:self.ptr + 4]
+                self.code[dest] = self.opval(mode, i, 0) * self.opval(mode, j, 1)
+                self.ptr += 4
+            elif op == 3:  # input
+                x = self.code[self.ptr + 1]
+                self.code[x] = self.invalues.pop(0)
+                self.ptr += 2
+            elif op == 4:  # output
+                x = self.code[self.ptr + 1]
+                outval = self.opval(mode, x, 0)
+                self.outvalues.append(outval)
+                self.ptr += 2
+                if return_output:
+                    self.returned_on = 'output'
+                    if self.outvalues:
+                        return self.outvalues[-1]
+                    else:
+                        return None
+            elif op == 5:  # jump-if-true
+                test, target = self.code[self.ptr + 1:self.ptr + 3]
+                if self.opval(mode, test, 0) != 0:
+                    self.ptr = self.opval(mode, target, 1)
+                else:
+                    self.ptr += 3
+            elif op == 6:  # jump-if-false
+                test, target = self.code[self.ptr + 1:self.ptr + 3]
+                if self.opval(mode, test, 0) == 0:
+                    self.ptr = self.opval(mode, target, 1)
+                else:
+                    self.ptr += 3
+            elif op == 7:  # less than
+                i, j, dest = self.code[self.ptr + 1:self.ptr + 4]
+                if self.opval(mode, i, 0) < self.opval(mode, j, 1):
+                    self.code[dest] = 1
+                else:
+                    self.code[dest] = 0
+                self.ptr += 4
+            elif op == 8:  # equals
+                i, j, dest = self.code[self.ptr + 1:self.ptr + 4]
+                if self.opval(mode, i, 0) == self.opval(mode, j, 1):
+                    self.code[dest] = 1
+                else:
+                    self.code[dest] = 0
+                self.ptr += 4
+            elif op == 99:  # terminate
+                self.returned_on = 'terminate'
+                if self.outvalues:
+                    return self.outvalues[-1]
+                else:
+                    return None
+            else:
+                assert False
 
-def opval(code, mode, val, rank):
-    return code[val] if (mode // (10 ** rank)) % 10 == 0 else val
-
+    def opval(self, mode, val, rank):
+        return self.code[val] if (mode // (10 ** rank)) % 10 == 0 else val
 
